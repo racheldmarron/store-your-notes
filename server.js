3b6510416e2f097913583fbac097d070f9500e0f
Rachel Marron <racheldmarron@gmail.com>
2022-03-08 13:30:40 -0800

    working on broken code

--- server.js
+++ server.js
@@ -1,60 +1,78 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs"); 
-const { allowedNodeEnvironmentFlags } = require("process");
-const { createBrotliDecompress } = require("zlib");
-// const util = require("util"); 
-const notes = JSON.parse(fs.readFileSync('./db/db.json', 'utf8')); 
+const util = require("util"); 
+// const { allowedNodeEnvironmentFlags } = require("process");
+// const { createBrotliDecompress } = require("zlib");
+// const data = require('./db/db.json'); 
+// const notes = JSON.parse(fs.readFileSync('./db/db.json', 'utf8')); 
 
-let app = express(); 
-let PORT = process.env.PORT || 3001; 
+const app = express(); 
+const PORT = process.env.PORT || 3001; 
 
-var createId = () => {
-    return 'id-' + Math.random().toString(36).substring(2, 16);
-  };
+// var createId = () => {
+//     return 'id-' + Math.random().toString(36).substring(2, 16);
+//   };
 
 app.use(express.urlencoded({ extended: true })); 
 app.use(express.json()); 
 
-app.use(express.static("./public"));
+app.use(express.static(path.join(__dirname, './public')));
 
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "./public/notes.html")); 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html')); 
 }); 
 
-app.get("/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "./public/notes.html")); 
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html')); 
 }); 
 
-app.get("/api/notes", (req, res) => {
-    console.log(notes);
-    return res.json(notes); 
+app.get('/api/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, 'db/db.json')); 
+}); 
+
+app.get("/api/notes/:note", function(req, res) {
+    var selectNote = req.params.note;
+    console.log(selectNote);
+    return res.json(selectNote) 
     }); 
 
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "./public/index.html")); 
+app.post("api/notes", (req, res) => {
+    const addNote = req.body; 
+    addNote.id = Date.now(); 
+    let noteData = fs.readFileSync('./db/db.json'); 
+    let noteTaker = JSON.parse(noteData); 
+
+    noteTaker.push(req.body);
+
+    fs.writeFileSync('./db/db.json', JSON.stringify(noteTaker), (err, data) => {
+        if (err) throw err;
+        res.json(noteTaker)
+    }); 
+
+    res.sendFile(path.join(__dirname, './public/notes.hmtl'));
 }); 
 
-app.post("api/notes", (req, res) => {
-    let addNote = req.body; 
-    addNote = createId(); 
-    notes.push(addNote); 
-    fs.writeFileSync("./db/db.json", JSON.stringify(notes)); 
-    res.json(notes); 
-})
-
 app.delete("/api/notes/:id", (req, res) => {
-    let deleteNote = req.params.id; 
+    let noteData = fs.readFileSync('./db/db.json');
+    let noteTaker = JSON.parse(noteData); 
 
-    for (let i = 0; i < notes.length; i++) {
-        if (deleteNote === notes[i].id) {
-            notes.splice(i,1);
-            fs.writeFileSync("./db/db.json", JSON.stringify(notes));
-            return res.json(notes); 
-        }
-    }
-    return res.json(false); 
-})
+    const noteSave = noteTaker.find(n.id === parseInt(req.params.id)); 
+
+    const noteIndex = noteTaker.indexOf(noteSave); 
+    noteTaker.splice(noteIndex); 
+
+    fs.writeFile(__dirname + "./db/db.json", JSON.stringify(noteTaker), (err, data) => {
+        if (err) throw err; 
+        res.json(noteTaker) 
+    });
+}); 
+
+app.get("/notes", (req, res) => 
+    res.sendFile(__dirname + "./public/notes.html")); 
+
+app.getMaxListeners("*", (req, res) =>
+    res.sendFile(__dirname + "./public/index.html")); 
 
 app.listen(PORT, () => {
     console.log(`App is listening at port ${PORT}`); 
