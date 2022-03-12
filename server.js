cb75b787794ba3d5c3a8510ffdae46b80e30f7d5
Rachel Marron <racheldmarron@gmail.com>
2022-03-12 10:43:42 -0800

    trying to fix broken code

--- server.js
+++ server.js
@@ -1,71 +1,107 @@
 const express = require("express");
 const path = require("path");
-const fs = require("fs"); 
+const fs = require("fs");
 
-const app = express(); 
-const PORT = process.env.PORT || 3001; 
 
-// let notesData = []; 
+const app = express();
 
-app.use(express.urlencoded({ extended: true })); 
-app.use(express.json()); 
-app.use(express.static("public")); 
-// app.use(express.static(path.join(__dirname, './public')));
+const PORT = process.env.PORT || 3001;
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, "public/index.html")); 
-}); 
+let notesData = [];
 
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, "public/notes.html")); 
-}); 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "/public")));
 
-app.get('/api/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, 'db/db.json')); 
-}); 
+app.get("/api/notes", function(err, res) {
+  try {
+    notesData = fs.readFileSync("/db/db.json", "utf8");
+    console.log("Hello world!");
+    notesData = JSON.parse(notesData);
 
-app.post("/api/notes", (req, res) => {
-    fs.readFile(path.join(__dirname, "db.json"), "utf8", function(error, response) {
-        if (error) {
-            console.log(error);
-        }
-        const notes = JSON.parse(response);
-        const noteAdd = req.body;
-        const newNoteID = notes.length + 1;
-        const newNote = {
-            id: newNoteID,
-            title: noteAdd.title,
-            text: noteAdd.text
-        };
-        notes.push(newNote);
-        res.json(newNote);
-        fs.writeFile(path.join(__dirname, "db.json"), JSON.stringify(notes, null, 2), function(err) {
-            if (err) throw err;
-        });
-    });
+  } catch (err) {
+    console.log("\n error (in app.get.catch):");
+    console.log(err);
+  }
+
+  res.json(notesData);
 });
 
-app.delete("/api/notes/:id", (req, res) => {
-    const deleteNote = req.params.id;
-    fs.readFile("db.json", "utf8", function(error, response) {
-        if (error) {
-            console.log(error);
-        }
-        let notes = JSON.parse(response);
-        if(deleteNote <= notes.length) {
-            res.json(notes.splice(deleteID-1,1));
-            for (let i = 0; i < notes.length; i++) {
-                notes[i].id = i+1;
-            }
-            fs.writeFile("db.json", JSON.stringify(notes, null, 2), function(err) {
-                if (err) throw err;
-            });
-        }else {
-            res.json(false);
-        }
+app.post("/api/notes", function(req, res) {
+  try {
+
+    notesData = fs.readFileSync("./db/db.json", "utf8");
+    console.log(notesData);
+
+ 
+    notesData = JSON.parse(notesData);
+
+    req.body.id = notesData.length;
+
+    notesData.push(req.body); 
+
+    notesData = JSON.stringify(notesData);
+
+    fs.writeFile("./db/db.json", notesData, "utf8", function(err) {
+
+      if (err) throw err;
     });
+
+    res.json(JSON.parse(notesData));
+
+
+  } catch (err) {
+    throw err;
+    console.error(err);
+  }
 });
 
-app.listen(PORT, () => {
-    console.log(`App is listening at port ${PORT}`); 
-}); 
+
+
+app.delete("/api/notes/:id", function(req, res) {
+  try {
+
+    notesData = fs.readFileSync("./db/db.json", "utf8");
+
+    notesData = JSON.parse(notesData);
+
+    notesData = notesData.filter(function(note) {
+      return note.id != req.params.id;
+    });
+
+    notesData = JSON.stringify(notesData);
+
+    fs.writeFile("./db/db.json", notesData, "utf8", function(err) {
+
+      if (err) throw err;
+    });
+
+
+    res.send(JSON.parse(notesData));
+
+
+  } catch (err) {
+    throw err;
+    console.log(err);
+  }
+});
+
+
+
+app.get("/notes", function(req, res) {
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+
+
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+app.get("/api/notes", function(req, res) {
+  return res.sendFile(path.json(__dirname, "/db/db.json"));
+});
+
+
+app.listen(PORT, function() {
+  console.log("Sever is listening on " + PORT);
+});
